2fb4e2457bddbbf96979a287780bf7d94e7530c4
Marcin Michnik <marcin.michnik@o2.pl>
2026-01-29 21:59:02 +0100

    Make the game easier

--- src/main/java/miterenewed/ModConstants.java
+++ src/main/java/miterenewed/ModConstants.java
@@ -10,8 +10,8 @@ public final class ModConstants {
     public static final int BASE_HUNGER = 3;
     public static final int LEVELS_PER_UPGRADE = 5;
 
-    public static final int REGEN_INTERVAL_TICKS = 1600;
+    public static final int REGEN_INTERVAL_TICKS = 1500;
     public static final float REGEN_AMOUNT = 1.0F;
-    public static final float PASSIVE_EXHAUSTION = 0.005f;
-    public static final float EXHAUSTION_ON_JUMP = 0.3f;
+    public static final float PASSIVE_EXHAUSTION = 0.0001f;
+    public static final float EXHAUSTION_ON_JUMP = 0.05f;
 }
